f65a53402badbd28e073ccfcc4bb64e3307f33ea
Mukund Ranjan <mukundranjan@gmail.com>
2014-10-27 01:02:55 +0530

    Final commit with more comments

--- cachematrix.R
+++ cachematrix.R
@@ -27,7 +27,6 @@ makeCacheMatrix <- function(origMtrx = matrix()) {
       )
 }
 
-
 ## This function computes the inverse of the special "matrix" returned by 
 ## makeCacheMatrix above. If the inverse has already been calculated (and the 
 ## matrix has not changed), then the cache solve should retrieve the inverse from
